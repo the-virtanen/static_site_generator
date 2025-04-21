16577b05f0248430aa182e85bc157ccfb0bd1098
the-virtanen <no email>
2025-04-21 10:55:43 +0300

    added class TextNode and TextType

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from textnode import *
 
-t1 = TextNode("hello", "text")
-t2 = TextNode("fuck","link","www.derp.com")
+def main():
+    t1 = TextNode("hello", "link", "https://www.boot.dev")
 
-print(repr(t1))
-print(repr(t2))
-print(t1 = t2)
+    print(t1)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,14 +9,14 @@ class TextType(Enum):
     IMAGE_TEXT = "image"
 
 class TextNode:
-    def __init__(self, text, text_type, url = NotImplemented):
+    def __init__(self, text, text_type, url = None):
         self.text = text
-        self.text_type = text_type
+        self.text_type = TextType(text_type)
         self.url = url
 
     def __eq__(self, compare):
         return self.text == compare.text and self.text_type == compare.text_type and self.url == compare.url
     
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     

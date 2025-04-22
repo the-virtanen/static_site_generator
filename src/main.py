fbf8abc3a91c4980df05a0b749347ffe7b413289
the-virtanen <no email>
2025-04-22 12:08:36 +0300

    added text_node_to_html_node

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
 from textnode import *
-from htmlnode import HTMLNode
+from htmlnode import *
+
 
 def main():
-    t1 = TextNode("hello", TextType.LINK, "https://www.boot.dev")
+    t1 = TextNode("hello", TextType.IMAGE, "https://www.boot.dev")
     h1 = HTMLNode( props={"href": "https://www.google.com","target": "_blank",})
-    print(h1)
+
+
+def text_node_to_html_node(text_node):
+     text = text_node.text
+     url = text_node.url
+
+     match text_node.text_type:
+          case TextType.TEXT:
+               return LeafNode(None, text)
+          case TextType.BOLD:
+               return LeafNode("b", text)
+          case TextType.ITALIC:
+               return LeafNode("i", text)
+          case TextType.CODE:
+               return LeafNode("code", text)
+          case TextType.LINK:
+               return LeafNode("a", text, {"href": url})
+          case TextType.IMAGE:
+               return LeafNode("img", None, {"src": url, "alt": url})
+          case _:
+               return("Incorrect text type")
+          
+    
 
 main()
---
+++ src/test_funcs.py
@@ -0,0 +1,21 @@
+import unittest
+from main import text_node_to_html_node
+from textnode import *
+
+class TestFuncsNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+
+    def test_link(self):
+        t1 = TextNode("hello", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(t1)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.props, {'href': 'https://www.boot.dev'})
+
+
+if __name__ == "__main__":
+    unittest.main()
